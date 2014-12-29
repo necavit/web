c153dd615494d33e2ab3e14e26444835671d6e8d
necavit <gollumdeagol@gmail.com>
2014-12-29 11:22:54 +0100

    working on the modal for cookie management

--- jekyll-static-site/js/cookie/cookie.mgr.js
+++ jekyll-static-site/js/cookie/cookie.mgr.js
@@ -1,10 +1,37 @@
 var POLICY_ACCEPT_COOKIE_NAME = "dmresacceptpolicy";
 
+function acceptCookiePolicy() {
+	console.log('Cookie policy accepted.');
+	$.cookie(POLICY_ACCEPT_COOKIE_NAME, 'true');
+}
+
+function rejectCookiePolicy() {
+	console.log('Cookie policy rejected.');
+	$.cookie(POLICY_ACCEPT_COOKIE_NAME, 'false');
+}
+
+function initCookiePolicyModal() {
+	$('#cookie-policy-modal-accept').click(function() {
+		acceptCookiePolicy();
+	});
+	$('#cookie-policy-modal-reject').click(function() {
+		rejectCookiePolicy();
+	});
+}
+
 function checkCookiePolicy() {
-	var acceptPolicyCookie = $.cookie('dmresacceptpolicy');
-	if (acceptPolicyCookie) {
-		console.log('Cookie policy accepted. Enabling analytics.');
-		alert('Enable Google Analytics');
+
+	initCookiePolicyModal();
+
+	var acceptCookies = $.cookie(POLICY_ACCEPT_COOKIE_NAME, JSON.parse);
+	if (typeof acceptCookies !== 'undefined') {
+		console.log('Cookie policy set with value: ' + acceptCookies);
+		if (acceptCookies) {
+			console.log('Enabling analytics.');
+		}
+		else {
+			console.log('Disabling analytics.');
+		}
 	}
 	else {
 		console.log('Cookie policy not yet accepted. Prompting user for acceptance.');
